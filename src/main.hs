ba2607144227b4bd9a2f59f8ed475cedde9ab44b
Miguel Fontes <miguel_m_fontes@hotmail.com>
2016-05-20 12:28:47 -0300

    Versão funcional!

--- src/main.hs
+++ src/main.hs
@@ -4,78 +4,51 @@ import Data.Time
 import System.Directory
 import System.IO
 import Control.Concurrent (threadDelay)
-import Control.DeepSeq
 
-type Arquivo = (String, String)
+data Arquivo = Arquivo {
+    nome :: String,
+    modificado :: String
+}
 
-getLastModified :: FilePath -> IO String
-getLastModified x = do
-    time <- fmap (formatTime defaultTimeLocale "%d/%m/%Y %T") (getModificationTime x)
-    return time
+instance Show Arquivo where
+    show (Arquivo n m) = "Arquivo: " ++ n ++ " - " ++ m
 
+instance Eq Arquivo where
+    (Arquivo n1 m1) == (Arquivo n2 m2) = (n1 == n2) && (m1 == m2)
 
-getLastModified' :: [FilePath] -> [String] -> IO [String]
-getLastModified' (f:fs) m = do
+getLastModified :: [FilePath] -> [String] -> IO [String]
+getLastModified (f:fs) m = do
     time <- fmap (formatTime defaultTimeLocale "%d/%m/%Y %T") (getModificationTime f)
-    if (length fs > 0)
-        then getLastModified' fs (time : m)
-        else return (time : m)
+    if not $ null fs
+        then getLastModified fs (time : m)
+        else return $ reverse (time : m)
 
+peek :: String -> [Arquivo] -> IO [Arquivo]
+peek name fl = do
+    when (name /= "") $ print name
+    print fl
+    return fl
 
-
-compareFileLists :: [(FilePath, IO String)] -> [(FilePath, IO String)] -> IO Bool
-compareFileLists [] _ = return False -- Checamos todos, nenhum mudou
-compareFileLists (l:[]) ul = fileChanged l ul -- Ultimo arquivo
-compareFileLists (l:ls) ul
-    | isNothing $ lookup (fst l) ul = return True -- Arquivo não existe, ou seja, e novo
-    | otherwise = fileChanged l ul >>= (\changed -> if changed then return changed else compareFileLists ls ul)
-
-fileChanged :: (FilePath, IO String) -> [(FilePath, IO String)] -> IO Bool
-fileChanged l ul = do
-    liftM2 (/=) (fromJust $ lookup (fst l) ul) (snd l)
-
-peek :: (FilePath, IO String) -> IO String
-peek fl = do
-    file <- return $ fst fl
-    time <- snd fl
-    t <- return $ (file ++ " - " ++ time)
-    print t
-    return t
-
-listaArquivos :: String -> IO [(FilePath, IO String)]
+listaArquivos :: String -> IO [Arquivo]
 listaArquivos dir = do
-    files <- getDirectoryContents dir
-    let parseFiles = map (\x -> dir ++ "/" ++ x) . filter (/=".") . filter (/="..")
-        parsedFilesList = parseFiles files
-        modification = map getLastModified parsedFilesList
-        fileMap = zip parsedFilesList modification
-    return fileMap
+    files <-  map (\x -> dir ++ "/" ++ x) . filter (\x -> x /= "." && x /="..")  <$> getDirectoryContents dir
+    modification <- getLastModified files []
+    return $ zipWith (Arquivo) files modification
 
-watch :: [(FilePath, IO String)] -> IO()
-watch ultLista = do
+watch :: [Arquivo] -> (() -> IO()) -> Int -> IO()
+watch ultLista action delay = do
+    threadDelay delay
+    print "-- Iteracao -----------------------------------------------------------------------------"
 
-    print "--> Ult Lista"
-    n <- return $ map peek ultLista
-    a1 <- last n
-    a2 <- head n
+    peek  "--> ultLista" ultLista
+    lista <- listaArquivos "." >>= peek "--> Lista"
 
-    threadDelay 3000000 -- Sem isso esse programa é um completo psicopata
-    print "------------------------------------------------------"
-
-    lista <- listaArquivos "."
-
-    print "--> Lista"
-    b <- return $ map peek lista
-    b1 <- last b
-    b2 <- head b
-
-    print $ (==) b2 a2
-
-    changed <- compareFileLists lista ultLista
-    if changed then print "Teve treta" else watch lista
+    if lista /= ultLista
+        then action () >> watch lista action delay
+        else watch lista action delay
 
 main :: IO()
 main  = do
     lista <- listaArquivos "."
-    watch lista
+    watch lista (\() -> print "############# ARQUIVOS ALTERADOS! ################") 3000000
     print "Complete!"
