0b5804a1b61cdf62b92b6e7a399e749a4c9fe82e
Miguel Fontes <miguel_m_fontes@hotmail.com>
2016-05-21 17:37:56 -0300

    Limpeza de COmentários

--- src/Arquivo/Watch.hs
+++ src/Arquivo/Watch.hs
@@ -9,18 +9,6 @@ import Action
 import Arquivo.Filter
 import Arquivo.Arquivo
 
-{-data Arquivo = Arquivo {
-    nome :: String,
-    modificado :: String,
-    dir :: String
-}
-
-instance Show Arquivo where
-    show (Arquivo n m d) = "Arquivo: " ++ n ++ " - Dir: " ++ d ++ " - Modificado:  " ++ m ++ "\n"
-
-instance Eq Arquivo where
-    (Arquivo n1 m1 d1) == (Arquivo n2 m2 d2) = (n1 == n2) && (m1 == m2) && (d1 == d2)-}
-
 watch :: [Arquivo] -> Action -> Int -> IO()
 watch ultLista action delay = do
     threadDelay delay
