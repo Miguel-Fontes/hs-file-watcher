0d282038b1c1c30102952938fe129c1f162567f4
Miguel Fontes <miguel_m_fontes@hotmail.com>
2016-05-22 13:32:37 -0300

    Novos campos de diretório na representação de arquivos

--- src/Arquivo/Arquivo.hs
+++ src/Arquivo/Arquivo.hs
@@ -3,11 +3,12 @@ module Arquivo.Arquivo where
 data Arquivo = Arquivo {
     nome :: String,
     modificado :: String,
-    dir :: String
+    dir :: String,
+    isDirectory :: Bool
 }
 
 instance Show Arquivo where
-    show (Arquivo n m d) = "Arquivo: " ++ n ++ " - Dir: " ++ d ++ " - Modificado:  " ++ m ++ "\n"
+    show (Arquivo n m d isD) = "Arquivo: " ++ n ++ " - Dir: " ++ d ++ " - Modificado:  " ++ m ++ " - isDir: " ++ (show isD) ++ "\n"
 
 instance Eq Arquivo where
-    (Arquivo n1 m1 d1) == (Arquivo n2 m2 d2) = (n1 == n2) && (m1 == m2) && (d1 == d2)
+    (Arquivo n1 m1 d1 _) == (Arquivo n2 m2 d2 _) = (n1 == n2) && (m1 == m2) && (d1 == d2)
