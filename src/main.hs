c7d9a2f1096a1c1d46b1d080a5679d2b795ac122
Miguel Fontes <miguel_m_fontes@hotmail.com>
2016-05-22 23:06:56 -0300

    Descrição de BUG

--- src/main.hs
+++ src/main.hs
@@ -5,7 +5,11 @@ import System.Environment
 
 main :: IO()
 main = do
-
+    -- BUG: Aplicação funcionando apenas com diretórios E quando o diretório é informado com um trailling \\
+    --      1 - Tratar argumento de entrada para identificar se é arquivo ou diretório
+    --      2a - Se for arquivo, obter o nome do arquivo e o caminho do diretório da String e construir o tipo Arquivo.
+    --      2b - Se for diretório, garantir que exista um trailling \\.
+    --      OBS: A necessidade do \\ é causada pelo uso da função setCurrentdirectory em Arquivo.Watch.listaArquivos
     let dir = "C:\\Desenv\\hs-file-watcher\\"
         filters = [excludeDirectories ["node_modules", ".git", "bower_components"]]
     args <- getArgs
