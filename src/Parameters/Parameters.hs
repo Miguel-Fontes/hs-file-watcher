839c361fbe9f57cd1c6f562fd159fdaaba790b22
Miguel Fontes <miguel_m_fontes@hotmail.com>
2016-05-24 15:48:16 -0300

    Protótipo de parsers

--- src/Parameters/Parameters.hs
+++ src/Parameters/Parameters.hs
@@ -3,9 +3,10 @@ module Parameters.Parameters where
 import Arquivo.Filter
 import Action
 
-data Parameters = Parameters {
-    directory :: String,
-    action :: [Action],
-    filters :: [Filter]
-}
+data Parameters = Parameters { directory :: FilePath
+                               , actions :: String
+                               , filters :: String }
 
+
+instance Show Parameters where
+    show (Parameters d a f) = show (d ++ " - " ++ a ++ " - " ++ f)
--- src/Parameters/Parsers.hs
+++ src/Parameters/Parsers.hs
@@ -4,28 +4,34 @@ import Parameters.Parameters
 import Arquivo.Filter
 import Action
 
-parseDir :: String -> String
-parseDir x = case takeWhile (=='\\') (reverse x) of
-                  [] -> x ++ "\\"
-                  (_:_) -> x
+formatDir :: String -> Parameters
+formatDir x = case takeWhile (=='\\') (reverse x) of
+                  [] -> Parameters { directory = x ++ "\\" }
+                  (_:_) -> Parameters { directory = x }
 
 parseFile :: String -> (String, String)
 parseFile x = case takeWhile (/='\\') (reverse x) of
                   [] -> ("", x)
                   xs -> (reverse xs, reverse $ drop (length xs) (reverse x))
 
-parseFilter :: String -> Filter
-parseFilter = excludeDirectories . words
+parseFilters :: (Parameters, String) -> Maybe (Parameters, String)
+parseFilters (p, xs) = Just (p { filters = fst fs }, snd fs)
+    where fs = getSection xs
 
-parseAction :: String -> Action
-parseAction = undefined
+parseAction :: (Parameters, String) -> Maybe (Parameters, String)
+parseAction (p, xs) = Just (p { actions = fst act }, snd act)
+    where act = getSection xs
 
-parseParameters :: String -> String
-parseParameters = undefined
+parseDir :: String -> Maybe (Parameters, String)
+parseDir xs = Just (formatDir (fst dir), snd dir)
+    where dir = getSection xs
 
 getSection :: String -> (String, String)
 getSection xs = (section, drop (length section + 1) xs)
     where section = takeWhile (/=' ') xs
+
+parseParameters :: String -> Maybe (Parameters, String)
+parseParameters xs = parseDir xs >>= parseAction >>= parseFilters
 
 
 
