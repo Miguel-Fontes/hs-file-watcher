1a6e9bde056322a4e474024f2a2ae97652ac1a49
Miguel Fontes <miguel_m_fontes@hotmail.com>
2016-05-24 19:41:26 -0300

    Encadeamento de parsers

--- src/Parameters/Parameters.hs
+++ src/Parameters/Parameters.hs
@@ -4,9 +4,11 @@ import Arquivo.Filter
 import Action
 
 data Parameters = Parameters { directory :: FilePath
-                               , actions :: String
-                               , filters :: String }
+                              ,actions   :: String
+                              ,filters   :: [Filter] }
 
+
+emptyParams = Parameters {directory = "", actions = "", filters = []}
 
 instance Show Parameters where
-    show (Parameters d a f) = show (d ++ " - " ++ a ++ " - " ++ f)
+    show (Parameters d a f) = show (d ++ " - " ++ a ++ " - " )
--- src/Parameters/Parsers.hs
+++ src/Parameters/Parsers.hs
@@ -4,34 +4,36 @@ import Parameters.Parameters
 import Arquivo.Filter
 import Action
 
-formatDir :: String -> Parameters
+formatDir :: String -> String
 formatDir x = case takeWhile (=='\\') (reverse x) of
-                  [] -> Parameters { directory = x ++ "\\" }
-                  (_:_) -> Parameters { directory = x }
+                  [] -> x ++ "\\"
+                  (_:_) -> x
 
 parseFile :: String -> (String, String)
 parseFile x = case takeWhile (/='\\') (reverse x) of
                   [] -> ("", x)
                   xs -> (reverse xs, reverse $ drop (length xs) (reverse x))
 
-parseFilters :: (Parameters, String) -> Maybe (Parameters, String)
-parseFilters (p, xs) = Just (p { filters = fst fs }, snd fs)
-    where fs = getSection xs
+parseFilters :: (Parameters, [String]) -> Maybe (Parameters, [String])
+parseFilters (p, []) = Just (p, [])
+parseFilters (p, x:xs)
+    | fst fs == "-ed" = let fs' = getSection (snd fs)
+                         in parseFilters (p { filters = excludeDirectory (fst fs') : filters p }, xs)
+    | otherwise = Nothing
+    where fs = getSection x
 
-parseAction :: (Parameters, String) -> Maybe (Parameters, String)
-parseAction (p, xs) = Just (p { actions = fst act }, snd act)
-    where act = getSection xs
+parseAction :: (Parameters, [String]) -> Maybe (Parameters, [String])
+parseAction (p, x:xs) = Just (p { actions = x }, xs)
 
-parseDir :: String -> Maybe (Parameters, String)
-parseDir xs = Just (formatDir (fst dir), snd dir)
-    where dir = getSection xs
+parseDir :: (Parameters, [String]) -> Maybe (Parameters, [String])
+parseDir (p, x:xs) = Just (p { directory = formatDir x }, xs)
 
 getSection :: String -> (String, String)
 getSection xs = (section, drop (length section + 1) xs)
     where section = takeWhile (/=' ') xs
 
-parseParameters :: String -> Maybe (Parameters, String)
-parseParameters xs = parseDir xs >>= parseAction >>= parseFilters
+parseParameters :: [String] -> Maybe (Parameters, [String])
+parseParameters xs = parseDir (emptyParams, xs) >>= parseAction >>= parseFilters
 
 
 
