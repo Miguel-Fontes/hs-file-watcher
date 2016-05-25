75ebb913a462764c39a7723d4ae3f16573a64dc9
Miguel Fontes <miguel_m_fontes@hotmail.com>
2016-05-24 21:22:32 -0300

    Integração do módulo Parameters ao Main.

--- src/main.hs
+++ src/main.hs
@@ -2,6 +2,9 @@ import Action
 import Arquivo.Watch
 import Arquivo.Filter
 import System.Environment
+import Parameters.Parameters
+import Parameters.Parsers
+import Data.Maybe
 
 main :: IO()
 main = do
@@ -10,9 +13,17 @@ main = do
     --      2a - Se for arquivo, obter o nome do arquivo e o caminho do diretório da String e construir o tipo Arquivo.
     --      2b - Se for diretório, garantir que exista um trailling \\.
     --      OBS: A necessidade do \\ é causada pelo uso da função setCurrentdirectory em Arquivo.Watch.listaArquivos
-    let dir = "C:\\Desenv\\hs-file-watcher\\"
-        filters = [excludeDirectories ["node_modules", ".git", "bower_components"]]
     args <- getArgs
-    lista <- listaArquivos filters dir
-    watch filters dir lista (textAction "############# ARQUIVOS ALTERADOS! ################") 3000000
+    print args
+
+    let --dir = "C:\\Desenv\\hs-file-watcher\\"
+        --f = [excludeDirectories ["node_modules", ".git", "bower_components"]]
+        p = fst $ fromJust (parseParameters args)
+        dir = directory p
+        fs = filters p
+
+    lista <- listaArquivos fs dir
+    watch fs dir lista (textAction "############# ARQUIVOS ALTERADOS! ################") 3000000
     print "Complete!"
+
+    -- Build ---> ghc -o ./../dist/hs-file-watcher main -odir ./../dist/ -hidir ./../dist/
