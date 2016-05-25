b2558c336acd11e8f118f25e326e0d0b37116a75
Miguel Fontes <miguel_m_fontes@hotmail.com>
2016-05-25 19:34:41 -0300

    Action para execução de programa externo

--- src/Action.hs
+++ src/Action.hs
@@ -1,5 +1,7 @@
 module Action where
 
+import System.Process
+
 newtype Action = Action (Tag,  () -> IO())
 
 type Tag= String
@@ -12,3 +14,6 @@ exec (Action (_, a)) = a
 
 textAction :: [String] -> Action
 textAction str = Action ("textAction: " ++ show str, \() -> print (unwords str))
+
+cmdAction :: [String] -> Action
+cmdAction cmd = Action ("cmdAction: " ++ show cmd, \() -> callCommand (concat cmd))
