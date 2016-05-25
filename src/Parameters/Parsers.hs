017bfce1560050f7ee749af68a6e290f1fdbb5a1
Miguel Fontes <miguel_m_fontes@hotmail.com>
2016-05-25 18:12:28 -0300

    Generalização de parser de options

--- src/Parameters/Parsers.hs
+++ src/Parameters/Parsers.hs
@@ -5,42 +5,40 @@ import Arquivo.Filter
 import Action
 
 import Data.List
-import Data.Maybe (isNothing, fromJust)
+import Data.Maybe (isNothing, isJust, fromJust)
 
-formatDir :: String -> String
-formatDir x = case takeWhile (=='\\') (reverse x) of
-                  [] -> x ++ "\\"
-                  (_:_) -> x
+parseParameters :: [String] -> Maybe (Parameters, [String])
+parseParameters xs = parseDir (emptyParams, xs) >>= parseOptions
+
+parseOptions :: (Parameters, [String]) -> Maybe (Parameters, [String])
+parseOptions (p, []) = Just (p, [])
+parseOptions (p, x:xs)
+    | isJust f = parseOptions (p{filters = fromJust f options : filters p }, drop (length options) xs)
+    | isJust a = parseOptions (p{actions = fromJust a options : actions p }, drop (length options) xs)
+    | otherwise = Nothing
+    where f = keyMatch x filtersList
+          a = keyMatch x actionsList
+          options = takeOptions xs
+
+parseDir :: (Parameters, [String]) -> Maybe (Parameters, [String])
+parseDir (p, x:xs) = Just (p { directory = formatDir x }, xs)
 
 parseFile :: String -> (String, String)
 parseFile x = case takeWhile (/='\\') (reverse x) of
                   [] -> ("", x)
                   xs -> (reverse xs, reverse $ drop (length xs) (reverse x))
 
-parseFilters :: (Parameters, [String]) -> Maybe (Parameters, [String])
-parseFilters (p, []) = Just (p, [])
-parseFilters (p, x:xs)
-    | isNothing f = Nothing
-    | otherwise = parseFilters (p{filters = fromJust f options : filters p}, drop (length options) xs)
-    where f = keyMatch x filtersList
-          options = takeOptions xs
-
-parseAction :: (Parameters, [String]) -> Maybe (Parameters, [String])
-parseAction (p, []) = Just (p, [])
-parseAction (p, x:xs)
-    | isNothing f = Just (p, x:xs)
-    | otherwise = parseAction (p{actions = fromJust f options : actions p }, drop (length options) xs)
-    where f = keyMatch x actionsList
-          options = takeOptions xs
-
-parseDir :: (Parameters, [String]) -> Maybe (Parameters, [String])
-parseDir (p, x:xs) = Just (p { directory = formatDir x }, xs)
-
-parseParameters :: [String] -> Maybe (Parameters, [String])
-parseParameters xs = parseDir (emptyParams, xs) >>= parseAction >>= parseFilters
-
 takeOptions :: [String] -> [String]
 takeOptions = takeWhile ((/='-') . head)
+
+formatDir :: String -> String
+formatDir x = case takeWhile (=='\\') (reverse x) of
+                  [] -> x ++ "\\"
+                  (_:_) -> x
+
+keyMatch :: String -> [([String], [String] -> a)] -> Maybe ([String] -> a)
+keyMatch _ [] = Nothing
+keyMatch op (f:fs) = if op `elem` fst f then Just (snd f) else keyMatch op fs
 
 filtersList :: [([String], [String] -> Filter)]
 filtersList = [(["--ed", "--exclude-directories"], excludeDirectories)
@@ -49,7 +47,3 @@ filtersList = [(["--ed", "--exclude-directories"], excludeDirectories)
 
 actionsList :: [([String], [String] -> Action)]
 actionsList = [(["--p", "--print"], textAction)]
-
-keyMatch :: String -> [([String], [String] -> a)] -> Maybe ([String] -> a)
-keyMatch _ [] = Nothing
-keyMatch op (f:fs) = if op `elem` fst f then Just (snd f) else keyMatch op fs
